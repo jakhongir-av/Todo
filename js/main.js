0ef531e102ed4eb6fae5e0094adacdc83fb1cc8f
Jakhongir AV <jakhongir.av@gmail.com>
2023-02-23 16:51:59 +0500

    Adding new todos

--- js/main.js
+++ js/main.js
@@ -21,49 +21,71 @@ btn.addEventListener("click", () => {
 
 
 //* Submit TODO
+
+const todos = [];
+const todo = [];
+
+
+async function render() {
+    tasksEl.innerHTML = '';
+    for(let i = 0; i <= todo.length; i++) {
+        const template = `
+            <div class="task">
+                <div class="content">
+                    <input 
+                        type="text" 
+                        class="text" 
+                        value="${todo[i].task}"
+                        readonly>
+                </div>
+                <div class="actions">
+                    <div class="form-check form-switch">
+                        <input 
+                            class="form-check-input"
+                            type="checkbox"
+                            role="switch"
+                            id="flexSwitchCheckChecked"
+                            ${todo[i].completed ? "checked" : ''}>
+                    </div>
+                    <button class="edit">Edit</button>
+                    <button class="delete">Delete</button>
+                </div>
+            </div>`; 
+        tasksEl.innerHTML = tasksEl.innerHTML + template;
+    }
+};
+
+
+
 newTaskFormEl.addEventListener("submit", (event) => {
     event.preventDefault();
 
-    let todo = newTaskInputEl.value;
 
-    const template = `
-    <div class="task">
-        <div class="content">
-            <input 
-                type="text" 
-                class="text" 
-                value="${todo}"
-                readonly>
-        </div>
-        <div class="actions">
-            <button class="edit">Edit</button>
-            <button class="delete">Delete</button>
-        </div>
-    </div>
-`;
-    tasksEl.innerHTML = template;
-
-    fetch("https://todo-for-n92.cyclic.app/todos/add",{
-        method: 'POST',
-        headers: {
-            "x-access-token": localStorage.getItem("token"),
-            'Content-Type':'application/json',
-        },
-        body: JSON.stringify({
-            task: todo,
+    let task = newTaskInputEl.value;
+    
+    
+    fetch("https://todo-for-n92.cyclic.app/todos/add", {
+            method: 'POST',
+            headers: {
+                "x-access-token": localStorage.getItem("token"),
+                'Content-Type':'application/json',
+            },
+            body: JSON.stringify({
+                task: task,
+            })
         })
-    })
-    .then((res) => res.json())
-    .then((res) => {
-        console.log(res);
-    })
-    .catch((err) => console.log(err));
+        .then((res) => res.json())
+        .then((res) => {
+            todo.push(res.todo);
+            render();
+            console.log(todo);        
+        })
+        .catch((err) => console.log(err));
+
+    newTaskInputEl.value = '';    
 
 });
 
 //* Delete TODO
 
-deleteBtnEl.addEventListener("click", () => {
-    
-})
 
