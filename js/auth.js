9a84d4ff6ae63b758c134fc8ad034329c4fab9c7
Jakhongir AV <jakhongir.av@gmail.com>
2023-02-25 14:02:52 +0500

    Delete todo completed

--- js/auth.js
+++ js/auth.js
@@ -2,39 +2,31 @@
 const signUpFormEl = document.querySelector("#signUpForm");
 const emailInputEl = document.querySelector("#emailInput");
 const passwordInputEl = document.querySelector("#passwordInput");
-const transferToLoginEl = document.querySelector(".transferToLogin")
+const transferToLoginEl = document.querySelector(".transferToLogin");
 
-signUpFormEl.addEventListener("submit",(evt)=>{
-    evt.preventDefault()
-    
+signUpFormEl.addEventListener("submit", (evt) => {
+  evt.preventDefault();
 
-    let bodyObj = {
-        username: emailInputEl.value,
-        password: passwordInputEl.value,
-    };
+  let bodyObj = {
+    username: emailInputEl.value,
+    password: passwordInputEl.value
+  };
 
-    fetch("https://todo-for-n92.cyclic.app/user/register",{
-        method: 'POST',
-        headers: {
-            'Content-Type':'application/json'
-        },
-        body: JSON.stringify(bodyObj)
+  fetch("https://todo-for-n92.cyclic.app/user/register", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(bodyObj)
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      if (data.token == undefined) {
+        alert("The user is already exists!");
+      } else {
+        localStorage.setItem("token", JSON.stringify(data.token));
+        window.location.replace("index.html");
+      }
     })
-    .then(res => res.json())
-
-    .then(data =>{
-        if(data.token == undefined) {
-            alert("The user is already exists!")
-        }else {
-            localStorage.setItem("token", JSON.stringify(data.token));
-            window.location.replace("index.html");
-        }
-        
-    }).catch(error=> console.log(error))
+    .catch((error) => console.log(error));
 });
-
-
-
-
-
-
--- js/main.js
+++ js/main.js
@@ -1,36 +1,32 @@
-const token = localStorage.getItem("token")
+const token = localStorage.getItem("token");
 const btn = document.querySelector(".logOutBtn");
 const tasksEl = document.querySelector("#tasks");
 const newTaskFormEl = document.querySelector("#new-task-form");
-const newTaskInputEl = document.querySelector("#new-task-input")
+const newTaskInputEl = document.querySelector("#new-task-input");
 const editBtnEl = document.querySelector(".edit");
 const deleteBtnEl = document.querySelector(".delete");
 
-
-
-
-if(!token) {
-    window.location.replace("signup.html");
+if (!token) {
+  window.location.replace("signup.html");
 }
 
-
 btn.addEventListener("click", () => {
-    localStorage.removeItem("token");
-    window.location.replace("signin.html");
+  localStorage.removeItem("token");
+  window.location.replace("signin.html");
 });
-
 
 //* Submit TODO
 
 const todos = [];
 let todo = [];
 
-
-async function render() {
-    tasksEl.innerHTML = '';
-    for(let i = 0; i <= todo.length; i++) {
-        const template = `
-            <div class="task">
+function render() {
+  tasksEl.innerHTML = "";
+  for (let i = 0; i < todo.length; i++) {
+    const template = `
+            <div class="task ${
+              todo[i].completed ? "bg-success text-light" : "bg-dark"
+            }">
                 <div class="content">
                     <input 
                         type="text" 
@@ -49,65 +45,92 @@ async function render() {
                             ${todo[i].completed && "checked"}>
                     </div>
                     <button class="edit">Edit</button>
-                    <button class="delete">Delete</button>
+                    <button class="delete" onclick="deleteTodo('${
+                      todo[i]._id
+                    }')">Delete</button>
                 </div>
-            </div>`; 
+            </div>`;
 
-        tasksEl.innerHTML = tasksEl.innerHTML + template;
-    };
-};
-
-
+    tasksEl.innerHTML = tasksEl.innerHTML + template;
+  }
+}
 
 newTaskFormEl.addEventListener("submit", (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
+  let task = newTaskInputEl.value;
 
-    let task = newTaskInputEl.value;
-    
-    
-    fetch("https://todo-for-n92.cyclic.app/todos/add", {
-            method: 'POST',
-            headers: {
-                "x-access-token": localStorage.getItem("token"),
-                'Content-Type':'application/json',
-            },
-            body: JSON.stringify({
-                task: task,
-            })
-        })
-        .then((res) => res.json())
-        .then((res) => {
-            todo.push(res.todo);
-            render();
-            console.log(res);        
-        })
-        .catch((err) => console.log(err));
+  fetch("https://todo-for-n92.cyclic.app/todos/add", {
+    method: "POST",
+    headers: {
+      "x-access-token": localStorage.getItem("token"),
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      task: task
+    })
+  })
+    .then((res) => res.json())
+    .then((res) => {
+      todo.push(res.todo);
+      render();
+    })
+    .catch((err) => console.log(err));
 
-    newTaskInputEl.value = '';    
-
+  newTaskInputEl.value = "";
 });
 
+//* Toggle complete function
 function toggleComplete(smth) {
-                fetch(`https://todo-for-n92.cyclic.app/todos?id=${smth}`, {
-                    method: "PUT",
-                    headers: {
-                            "x-access-token": localStorage.getItem("token")
-                    },
-                }) 
-                .then((res) => res.json())
-                .then((res) =>{
-                    console.log(res);
-                    todo = todo.map((id) => {
-                        if(id._id === res.todo._id) {
-                            todo[id] = res.todo;  
-                        }else {
-                            todo[id] = id
-                        };
-                        return todo[id];
-                    });
-                    console.log(todo);
-                    render();
-                })
-                .catch((err) => console.log(err));
-};
+  fetch(`https://todo-for-n92.cyclic.app/todos?id=${smth}`, {
+    method: "PUT",
+    headers: {
+      "x-access-token": localStorage.getItem("token")
+    }
+  })
+    .then((res) => res.json())
+    .then((res) => {
+      console.log(res);
+      todo = todo.map((id) => {
+        if (id._id === res.todo._id) {
+          todo[id] = res.todo;
+        } else {
+          todo[id] = id;
+        }
+        return todo[id];
+      });
+      render();
+    })
+    .catch((err) => console.log(err));
+}
+
+//* Delete todo
+
+function deleteTodo(id) {
+  const isAccepted = confirm("Are you sure to delete??");
+  if (isAccepted) {
+    fetch(`https://todo-for-n92.cyclic.app/todos/${id}`, {
+      method: "DELETE",
+      headers: {
+        "x-access-token": token
+      }
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        todo = todo.filter((todo) => {
+          if (
+            id === res.deletedTodo._id &&
+            res.deletedTodo._id === todo._id &&
+            id === todo._id
+          ) {
+            return null;
+          } else {
+            return todo;
+          }
+        });
+
+        render();
+      })
+      .catch((error) => console.log(error));
+  }
+}
